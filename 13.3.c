eb3c89ca89c9258e865fef8630a97e1f502fbf6c
GreenAlex7 <57642040+GreenAlex7@users.noreply.github.com>
2020-03-13 11:12:48 +0300

    Homework belong to student of group 1141
     Kupinskiy Alexey Viktorovich

---
+++ 13.3.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h> 
+#include <stdio.h> 
+#include <conio.h> 
+#include <malloc.h>
+#define for_i for(int i = 0; i < n; i++) 
+
+void Bubble(int* a, int* b)
+{
+	int o = *a;
+	*a = *b;
+	*b = o;
+
+}
+
+void av(int n, int* e)
+{
+	int c = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 1; j < (n - i); j++)
+		{
+
+			if (e[j] < e[j - 1])
+			{
+				Bubble(&e[j], &e[j - 1]);
+			}
+			c++;
+		}
+
+	}
+	printf("Number of comparisons:");
+	printf("%d\n", c);
+}
+
+
+
+
+int main()
+{
+	int n;
+	printf("Array's size:");
+	scanf("%d", &n);
+	int* e = (int*)calloc(n, sizeof(int));
+	printf("Array:");
+	for_i
+	{
+	e[i] = rand()%100;
+	printf("%d ", e[i]);
+	
+	}
+	printf("\n");
+	av(n, e);
+	printf("Bubbled array:");
+	for_i
+	{
+	printf("%d ", e[i]);
+	}
+	free(e);
+}
